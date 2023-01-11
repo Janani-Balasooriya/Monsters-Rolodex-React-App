bc632667ccb1759e3e684f3f6e0300f7ff4fbf0b
Janani-Balasooriya <jananidev7@gmail.com>
2023-01-11 20:28:19 +0530

    fetch users done

--- src/App.js
+++ src/App.js
@@ -8,18 +8,38 @@ class App extends Component {
     super();
 
     this.state = {
-      name: "Janani",
+      monsters: [],
     };
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
+    console.log("render");
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello {this.state.name}</p>
-          <button>Change Name</button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
